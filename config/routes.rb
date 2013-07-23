2bf466536fe2e5ee6e610f31ebbae3a13f75c698
noam <clusterfoo@server.fake>
2013-07-23 19:27:59 -0400

    starting user page and form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 MemoRabble::Application.routes.draw do
+  resources :users
+
+  match '/signup', to: 'users#new', via: 'get'
+
+  # Static Pages:
   root 'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contribute', to: 'static_pages#contribute', via: 'get'
-  match '/signup', to: 'users#new', via: 'get'
 end
--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -6,6 +6,13 @@ describe "User pages" do
   describe "Signup page" do
     before { visit signup_path }
     it { should have_content("Sign up") }
-    it { should have_title('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+
+  describe "Profile page" do
+    before { visit user_path(user) }
+
+    it { should have_title(full_title(user.username)) }
+    it { should have_content(user.username) }
   end
 end
