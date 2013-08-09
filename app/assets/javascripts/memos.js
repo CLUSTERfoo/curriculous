0d6ef96be2f407a13727d0d08d956e49f31a6feb
noam <clusterfoo@server.fake>
2013-08-08 20:24:15 -0400

    memo fades in.

--- app/assets/javascripts/memos.js
+++ app/assets/javascripts/memos.js
@@ -1,5 +1,5 @@
 $(document).ajaxSuccess(function(e, content){
     console.log(content);
     
-    $("#parent-memo").html(content.responseText);
+    $("#parent-memo").html(content.responseText).hide().fadeIn(1000);
 });
