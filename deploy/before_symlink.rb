8fcaa6a2619eb0b02a2886c3651c7f680104092c
noam <clusterfoo@server.fake>
2013-10-06 15:37:06 -0400

    card 11 -- config.*_path

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,3 +1,3 @@
 # Symlink .secret token file so sessions don't get destroyed.
-run "mkdir -p #{ shared_path }/my_shared"
-run "ln -nfs #{ shared_path }/my_shared/.secret #{ release_path }/.secret"
+run "mkdir -p #{ config.shared_path }/my_shared"
+run "ln -nfs #{ config.shared_path }/my_shared/.secret #{ config.release_path }/.secret"
