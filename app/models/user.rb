f906ee0a9016b3dc03140a48af91959eee2f1a74
noam <clusterfoo@server.fake>
2013-10-06 16:05:34 -0400

    card 9 -- FIX: can't update users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  after_validation { self.display_name = username }
-  before_save { self.username = username.downcase }
+  before_save do 
+    self.display_name = username if self.display_name == nil
+    self.username = username.downcase
+  end
 
   has_many :memos, dependent: :destroy
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
-  # TODO: get actual email validation.
+# # TODO: get actual email validation.
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX, allow_blank: true }
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password
